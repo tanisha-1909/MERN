75b7eb442a278e21d0c3f92817eb9c75f1606f97
tanisha-1909 <tanishamahavar32@gmail.com>
2024-11-18 11:28:41 +0530

    frontend setup

--- Server/index.js
+++ Server/index.js
@@ -4,6 +4,34 @@ const app =express(); // app just a variable and store express function
 
 const PORT = 8080; // any value
 
-app.listen(PORT, () => {
-    console.log("Server Started at port "+ PORT)
+const names = [
+    {
+        id: 1,
+        name: "Yash1",
+    },
+    {
+        id: 2,
+        name: "Yash2",
+    },
+];
+
+app.get("/names", (req, res) => {
+    res.send(names);
 })
+
+app.post("/add", (req,res) =>{
+    const newName = req.body();
+    const dataToAdd = new names(newName);
+    dataToAdd.save();
+    res.send(names);
+})
+
+app.delete("/delete/:id", (req,res) =>{
+    const idToDelete = req.params.id;
+    names.findByIdAndDelete({ idToDelete });
+    res.send(names);
+})
+
+// app.listen(PORT, () => {
+//     console.log("Server Started at port "+ PORT)
+// })
